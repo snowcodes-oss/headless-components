7f8a3711e617a79589ed6c0e027cb6e5fc9102d8
Alexandre <sensory_upshot_0e@icloud.com>
2023-02-16 23:08:43 +0100

    feat(button): update documentation

--- projects/demo/src/app/components/button/button.component.ts
+++ projects/demo/src/app/components/button/button.component.ts
@@ -5,4 +5,14 @@ import { Component } from "@angular/core";
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent { }
+export class ButtonComponent {
+
+  usageImport = 'import { HeadlessButtonModule } from "@snowcodes/headless-components/button";';
+  usageHtml = '<headless-button>Hello world</headless-button>';
+  usageHtmlType = `<headless-button type="submit | reset | button">Button with different type</headless-button>`;
+  usageHtmlDisabled = `<headless-button disabled>Disabled button</headless-button>`;
+  usageHtmlCustom = `<headless-button override="custom-button-color">With custom color</headless-button>`;
+
+  usageHtmlColor = `<headless-button color="primary | secondary | ...">Your button</headless-button>`;
+  usageHtmlOutlined = `<headless-button appearance="default | outlined" color="...">Your button</headless-button>`;
+}
